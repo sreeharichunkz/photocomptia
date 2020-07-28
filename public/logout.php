2669cbf5d315ba0a37920c349dc43c108ba648a3
Sreehari <sreeharichunkz@gmail.com>
2020-07-28 19:53:29 +0530

    public file deleted

--- public/logout.php
+++
@@ -1,6 +0,0 @@
-<?
-session_start();
-unset($_SESSION['adminname']);
-
-header('Location: admin.php');
-   ?>
--- public/pdo.php
+++
@@ -1,4 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=photo', 'fred', 'zap');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
